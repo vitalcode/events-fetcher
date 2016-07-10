d5080ef6b081bac1316e635ed1aa206f2287a08e
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2016-07-10 17:52:48 +0100

    Remove meaningless text characters while fetching the page text data

--- src/main/scala/uk/vitalcode/events/fetcher/parser/textParser.scala
+++ src/main/scala/uk/vitalcode/events/fetcher/parser/textParser.scala
@@ -9,7 +9,7 @@ object TextParser extends ParserLike[String] {
         .map(_.replaceAll(notAllowedCharasters, " ")
             // CamelCase to Camel Case Text: http://stackoverflow.com/questions/2559759/how-do-i-convert-camelcase-into-human-readable-names-in-java
             .replaceAll("""(?<=[A-Z])(?=[A-Z][a-z])|(?<=[^A-Z0-9,.\/\\<>?!|"'’`:;{}\[\]=+-_()@$#£%^&*~±])(?=[A-Z])""", " ")
-            .replaceAll( """\s{2,}""", " ")
+            .replaceAll( """(\s{2,})|(_{2,})""", " ")
             .replaceAll( """^\s|\s$""", "")
         )
         .filter(_.nonEmpty)
