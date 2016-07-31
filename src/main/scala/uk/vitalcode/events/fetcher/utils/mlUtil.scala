a59fd700244fa924a328e5b5e81cd9ed661d51d9
Vitaliy Kuznetsov <vitaliy.kuznetsov@yahoo.co.uk>
2016-07-31 17:18:27 +0100

    vip2

--- src/main/scala/uk/vitalcode/events/fetcher/utils/mlUtil.scala
+++ src/main/scala/uk/vitalcode/events/fetcher/utils/mlUtil.scala
@@ -60,8 +60,13 @@ object MLUtil {
 
         import sqlContext.implicits._
 
-        val trainPath = Path(MLUtil.getClass.getResource("/").getPath)./("EventCategoryTrain/*").toString()
-        val trainRDD: RDD[(String, String)] = sqlContext.sparkContext.wholeTextFiles(trainPath.toString)
+
+        val tr = MLUtil.getClass.getResource("/")
+
+        val trainPath = s"${tr.getProtocol}:${tr.getPath}EventCategoryTrain/*"
+
+        //val trainPath = Path(MLUtil.getClass.getResource("/").getPath)./("EventCategoryTrain/*").toString()
+        val trainRDD: RDD[(String, String)] = sqlContext.sparkContext.wholeTextFiles(trainPath)
         val data = trainRDD.map {
             case (file, text) => (Category.withName(file.split("/").takeRight(2).head.toUpperCase).id.toDouble, text)
         }.toDF
